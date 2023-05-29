882f5fadc241779ee844545628dcd4e70abf7fb4
SergeyMihailovich <fsm_mail.ru>
2023-05-29 23:54:26 +0300

    add resources

---
+++ resources/data.py
@@ -0,0 +1,27 @@
+pages_url = {
+    'main_page': 'https://reqres.in',
+    'users': '/api/users',
+    'unknown': '/api/unknown',
+    'register': '/api/register',
+    'login': '/api/login'
+}
+
+select_button = {
+    'res_code': '.response-code',
+    'res_body': '[data-key="output-response"]',
+    'list_users': '[data-id="users"]',
+    'single_user': '[data-id="users-single"]',
+    'single_user_not_found': '[data-id="users-single-not-found"]',
+    'list_resource': '[data-id="unknown"]',
+    'single_resource': '[data-id="unknown-single"]',
+    'single_resource_not_found': '[data-id="unknown-single-not-found"]',
+    'create': '[data-id="post"]',
+    'update': '[data-id="put"]',
+    'patch_update': '[data-id="patch"]',
+    'delete': '[data-id="delete"]',
+    'register_successful': '[data-id="register-successful"]',
+    'register_unsuccessful': '[data-id="register-unsuccessful"]',
+    'login_successful': '[data-id="login-successful"]',
+    'login_unsuccessful': '[data-id="login-unsuccessful"]',
+    'delayed_response': '[data-id="delay"]'
+}
